91d80b3eb1d43f2756edab3d7696d0345389430a
Mason Silber <mdsilber1@gmail.com>
2015-01-21 19:49:04 -0500

    Added activity names to data set

--- Dataset/run_analysis.R
+++ Dataset/run_analysis.R
@@ -12,45 +12,55 @@ mergeTwoFiles <- function(firstFileName, secondFileName) {
     write.table(mergedFile, paste("merged", mergedFileBaseName, sep="/"));
 }
 
-# step 1 - merge files
-testFileNames <- list.files("test", pattern = "*.txt");
-trainFileNames <- list.files("train", pattern = "*.txt");
-
-filePairs <- cbind(testFileNames, trainFileNames);
-
-if (!file.exists("merged")) {
-    dir.create("merged");
-    for (row in 1:nrow(filePairs)) {
-        firstFile <- filePairs[row,][[1]];
-        secondFile <- filePairs[row,][[2]];
-        mergeTwoFiles(firstFile, secondFile);    
-    }
-}
-
-print("Merged!!");
-
-# step 2 - extract means and standard deviations
-features <- read.table(".features.txt")$V2;
-meanAndStdevRegEx <- "(-mean\\(\\))|(-std\\(\\))";
-meansAndStdevs <- as.logical(sapply(features, function(f) { grepl(meanAndStdevRegEx, f); }));
-
-fileData <- read.table("merged/X_merged.txt");
-fileDataSubset <- fileData[, meansAndStdevs];
-
-print("Mean and standard deviation extracted!");
-
-# step 3 - Name activities meaningfully and merge them into the table
-
-# step 4 - Name columns meaningfully
-
-columnNames <- unlist(sapply(features[meansAndStdevs], function(x) {
-    splitString <- sapply(strsplit(as.character(x), "-"), function(y) {
-        firstLetter <- substring(y, 1, 1);
-        tidiedString <- gsub("\\(\\)", "", substring(y, 2, nchar(y)));
-        paste(toupper(firstLetter), tidiedString, sep="");
-    });
+run_analysis <- function() {
+    # step 1 - merge files
+    testFileNames <- list.files("test", pattern = "*.txt");
+    trainFileNames <- list.files("train", pattern = "*.txt");
     
-    paste(splitString[,1], collapse="");
-}))
+    filePairs <- cbind(testFileNames, trainFileNames);
+    
+    if (!file.exists("merged")) {
+        dir.create("merged");
+        for (row in 1:nrow(filePairs)) {
+            firstFile <- filePairs[row,][[1]];
+            secondFile <- filePairs[row,][[2]];
+            mergeTwoFiles(firstFile, secondFile);    
+        }
+    }
+    
+    print("Merged!!");
+    
+    # step 2 - extract means and standard deviations
+    features <- read.table("features.txt")$V2;
+    meanAndStdevRegEx <- "(-mean\\(\\))|(-std\\(\\))";
+    meansAndStdevs <- as.logical(sapply(features, function(f) { grepl(meanAndStdevRegEx, f); }));
+    
+    fileData <- read.table("merged/X_merged.txt");
+    fileDataSubset <- fileData[, meansAndStdevs];
+    
+    print("Mean and standard deviation extracted!");
+    
+    # step 3 - Name activities meaningfully and merge them into the table
+    
+    activity_labels <- c("Laying", "Sitting", "Standing", "Walking", "Walking Downstairs", "Walking Upstairs");
+    unnamedActivities <- read.table("merged/y_merged.txt")[,1];
+    namedActivities <- sapply(unnamedActivities, function(x) { activity_labels[x] });
+    fileDataSubset <- cbind(namedActivities, fileDataSubset);
 
-names(fileDataSubset) <- columnNames;
+    print("Activities named!");
+
+    # step 4 - Name columns meaningfully
+    
+    columnNames <- unlist(sapply(features[meansAndStdevs], function(x) {
+        splitString <- sapply(strsplit(as.character(x), "-"), function(y) {
+            firstLetter <- substring(y, 1, 1);
+            tidiedString <- gsub("\\(\\)", "", substring(y, 2, nchar(y)));
+            paste(toupper(firstLetter), tidiedString, sep="");
+        });
+        
+        paste(splitString[,1], collapse="");
+    }))
+    
+    names(fileDataSubset) <- c("Activity", columnNames);
+    print ("Columns named!");
+}
